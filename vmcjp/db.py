bb3366307acc8cd3fff62874323b10b05b94857c
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-12 21:20:09 +0900

    Update db.py

--- vmcjp/db.py
+++ vmcjp/db.py
@@ -22,8 +22,8 @@ def get_event_collection(url):
 def get_cred_collection(url):
     return get_cred_db(url)[constant.CRED_COLLECTION]
 
-#def _read_event_db(url, user_id, minutes=None):
-def read_event_db(url, user_id, minutes=None):
+def _read_event_db(url, user_id, minutes=None):
+#def read_event_db(url, user_id, minutes=None):
     event_col =get_event_collection(url)
 
     if minutes is None:
@@ -36,50 +36,50 @@ def read_event_db(url, user_id, minutes=None):
         
     return data
 
-#def read_event_db(event):
-#    return _read_event_db(event.get("db_url"), event.get("user_id"), event.get("minutes"))
+def read_event_db(event):
+    return _read_event_db(event.get("db_url"), event.get("user_id"), event.get("minutes"))
     
-#def _read_cred_db(url, user_id):
-def read_cred_db(url, user_id):
+def _read_cred_db(url, user_id):
+#def read_cred_db(url, user_id):
     cred_col = get_cred_collection(url)
     return cred_col.find_one({"_id": user_id})
 
-#def read_cred_db(event):
-#    return _read_cred_db(event.get("db_url"), event.get("user_id"))
+def read_cred_db(event):
+    return _read_cred_db(event.get("db_url"), event.get("user_id"))
 
-#def _write_event_db(url, user_id, data):
-def write_event_db(url, user_id, data):
+def _write_event_db(url, user_id, data):
+#def write_event_db(url, user_id, data):
     event_col = get_event_collection(url)
     now = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
     data.update({"start_time": now})
     event_col.update({"_id": user_id}, {"$set": data}, upsert=True)
 
-#def write_event_db(event):
-#    _write_event_db(event.get("db_url"), event.get("user_id"), event.get("data"))
+def write_event_db(event):
+    _write_event_db(event.get("db_url"), event.get("user_id"), event.get("data"))
 
-#def _write_cred_db(url, user_id, data):
-def write_cred_db(url, user_id, data):
+def _write_cred_db(url, user_id, data):
+#def write_cred_db(url, user_id, data):
     cred_col = get_cred_collection(url)
     cred_col.update({"_id": user_id}, {"$set": data}, upsert=True)
 
-#def write_cred_db(event):
-#    _write_cred_db(event.get("db_url"), event.get("user_id"), event.get("data"))
+def write_cred_db(event):
+    _write_cred_db(event.get("db_url"), event.get("user_id"), event.get("data"))
 
-#def _delete_event_db(url, user_id):
-def delete_event_db(url, user_id):
+def _delete_event_db(url, user_id):
+#def delete_event_db(url, user_id):
     event_col = get_event_collection(url)
     event_col.remove({"_id": user_id})
 
-#def delete_event_db(event):
-#    _delete_event_db(event.get("db_url"), event.get("user_id"))
+def delete_event_db(event):
+    _delete_event_db(event.get("db_url"), event.get("user_id"))
 
-#def _delete_cred_db(url, user_id):
-def delete_cred_db(url, user_id):
+def _delete_cred_db(url, user_id):
+#def delete_cred_db(url, user_id):
     cred_col = get_cred_collection(url)
     cred_col.remove({"_id": user_id})
 
-#def delete_cred_db(event):
-#    _delete_cred_db(event.get("db_url"), event.get("user_id"))
+def delete_cred_db(event):
+    _delete_cred_db(event.get("db_url"), event.get("user_id"))
     
 def lambda_handler(event, context):
 #    logging.info("user_id: {}".format(event.get("user_id")))
